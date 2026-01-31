ae78b3babd99c005802c4882f27edb4a17d09920
Siena Hurwitz <shurwitz@umd.edu>
2026-01-30 20:05:53 -0500

    getting prepared to make a nice demo for it

--- debugger.py
+++ debugger.py
@@ -1,13 +1,9 @@
-import desc
 import inspect
-import linecache
 import numpy as np
 import sys
 import threading
 import time
 import traceback
-
-from hey import abc
 
 def siena_debugger(func):
     def wrapper(*args, **kwargs):
@@ -100,30 +96,3 @@ def print_tree(namestack_old, namestack_new):
             print("| " * i + "* " + namestack_new[i])
             i += 1
 
-@siena_debugger
-def heavy_lifting():
-    x = 10
-    time.sleep(4.9) # Simulate a slow operation
-    g()
-    y = 20
-    h()
-    abc()
-    z = x + y
-    np.abs([-1, 1, 3])
-    b=1
-    
-    # desc.set_device('cpu')
-    return z
-
-def g():
-    time.sleep(1.2)
-    return 0
-
-def h():
-    time.sleep(0.3)
-    return l()
-
-def l():
-    return "haha"
-
-heavy_lifting()
