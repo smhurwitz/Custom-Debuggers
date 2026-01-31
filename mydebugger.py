f119ab82c0678ceebe1f5a08f9e461b77debb35f
Siena Hurwitz <shurwitz@umd.edu>
2026-01-30 19:57:30 -0500

    it works!!!

--- mydebugger.py
+++ mydebugger.py
@@ -7,6 +7,8 @@ import threading
 import time
 import traceback
 
+from hey import abc
+
 def siena_debugger(func):
     def wrapper(*args, **kwargs):
         thread_2 = None
@@ -38,18 +40,22 @@ def siena_debugger(func):
             if stop_event is not None:
                 stop_event.set()
                 thread_2.join()
-
-            stack_new = traceback.extract_stack(frame)
-            # fmt_trace = traceback.format_list([stack[-1]])[0]
-
-            
-
             
             ###### -----------------------------------
-            #TODO: define stacktrace_new = stack
-            # extract filenamestack from old and new stacktrace, if old is None then []
-            # pass to print_tree
+            nonlocal stack_old
+            stack_new = traceback.extract_stack(frame)
 
+            #TODO 2) extract filenamesstacks for old and new; if old is None then []
+            if stack_old is None:
+                filesstack_old = np.array([""])
+            else:
+                filesstack_old = np.array([f"{f.filename} {f.name}()" for f in stack_old])
+            filesstack_new = np.array([f"{f.filename} {f.name}()" for f in stack_new])
+
+            #TODO 3) pass to print_tree
+            print_tree(filesstack_old, filesstack_new)
+
+            stack_old = stack_new
             #### ------------------------------------
 
 
@@ -57,10 +63,8 @@ def siena_debugger(func):
             end_line = start_line + len(lines) - 1
 
             if frame.f_lineno < end_line: #set another timer...
-                func_name = frame.f_code.co_name
-                fileloc = frame.f_code.co_filename
                 lineno = frame.f_lineno
-                premod = f"{fileloc} | function: {func_name}(); Line {lineno}: "
+                premod = "| " * len(filesstack_new) + f"Line {lineno}: "
                 stop_event = threading.Event()
                 thread_2 = threading.Thread(target=timer, args=(stop_event, premod, ))
                 thread_2.start()
@@ -73,17 +77,6 @@ def siena_debugger(func):
             sys.settrace(None)
     return wrapper
 
-@siena_debugger
-def heavy_lifting():
-    x = 10
-    time.sleep(0.5) # Simulate a slow operation
-    y = 20
-    z = x + y
-    # desc.set_device('cpu')
-    return z
-
-# heavy_lifting()
-
 # prints progression through a tree by comparing an old and new stacktrace
 def print_tree(namestack_old, namestack_new):
     # calculate lengths of the stacks
@@ -95,9 +88,9 @@ def print_tree(namestack_old, namestack_new):
         namestack_old = np.pad(namestack_old, (0, l_new-l_old), mode='constant')
     elif l_new < l_old:
         namestack_new = np.pad(namestack_new, (0, l_old-l_new), mode='constant')
-
     # calculate number of shared nodes
-    n = np.argwhere((namestack_old == namestack_new) == False)[:, 0][0]
+    argwhere = np.argwhere((namestack_old == namestack_new) == False)
+    n = l_new if argwhere.size == 0 else argwhere[:, 0][0]
     
     if n < l_old: # if the tree backtracks
         print("| " * n + "- " * (l_old - n))
@@ -107,13 +100,30 @@ def print_tree(namestack_old, namestack_new):
             print("| " * i + "* " + namestack_new[i])
             i += 1
 
-stack0 = []
-stack1 = ["a", "b", "c"]
-stack2 = ["a", "b", "c", "d", "e"]
-stack3 = ["a", "f", "g"]
-stack4 = ["a", "f"]
+@siena_debugger
+def heavy_lifting():
+    x = 10
+    time.sleep(4.9) # Simulate a slow operation
+    g()
+    y = 20
+    h()
+    abc()
+    z = x + y
+    np.abs([-1, 1, 3])
+    b=1
+    
+    # desc.set_device('cpu')
+    return z
 
-print_tree(stack0, stack1)
-print_tree(stack1, stack2)
-print_tree(stack2, stack3)
-print_tree(stack3, stack4)
+def g():
+    time.sleep(1.2)
+    return 0
+
+def h():
+    time.sleep(0.3)
+    return l()
+
+def l():
+    return "haha"
+
+heavy_lifting()
